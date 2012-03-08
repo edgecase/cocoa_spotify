b389bdb1453005b1367d2fd32759e5e0a3cf7ff6
Tony Schneider <tonywok@gmail.com>
2012-03-08 11:10:22 -0500

    Initial Commit

---
+++ CoccoaSpotbox/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  CoccoaSpotbox
+//
+//  Created by Tony Schneider on 3/8/12.
+//  Copyright (c) 2012 Edgecase. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
