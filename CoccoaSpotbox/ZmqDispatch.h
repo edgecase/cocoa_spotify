4478a2af6be34c361537b475e0b0ffc006cd74f5
Tony Schneider <tonywok@gmail.com>
2012-03-12 01:06:19 -0400

    add git ignore

---
+++ CoccoaSpotbox/ZmqDispatch.h
@@ -0,0 +1,13 @@
+//
+//  ZmqDispatch.h
+//  CoccoaSpotbox
+//
+//  Created by Tony Schneider on 3/8/12.
+//  Copyright (c) 2012 Edgecase. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZmqDispatch : NSObject
+
+@end
---
+++ CoccoaSpotbox/ZmqDispatch.m
@@ -0,0 +1,13 @@
+//
+//  ZmqDispatch.m
+//  CoccoaSpotbox
+//
+//  Created by Tony Schneider on 3/8/12.
+//  Copyright (c) 2012 Edgecase. All rights reserved.
+//
+
+#import "ZmqDispatch.h"
+
+@implementation ZmqDispatch
+
+@end
